5be453e220a1fac22697ecad79bdebd415de0cd3
boldogdavid <118424856+boldogdavid@users.noreply.github.com>
2022-12-01 11:46:06 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from tkinter import *
+import ado_szamol
+
+def indul():
+    netto.insert(END, str(ado_szamol.nettok(int(ber.get()))))
+    osszado.insert(END, str(ado_szamol.adok(int(ber.get()))))
+
+
+
+window = Tk()
+window.title("Adó számolás!")
+Label(window,text = "bruttó bér").grid(row = 0,column = 0)
+
+ber = Entry(window, width=10 )
+ber.grid(row=0,column=1)
+Button(window, text="Számol", command=indul).grid(row=1,column=0)
+
+Label(window,text="nettó bér").grid(row=2,column=0)
+netto=Entry(window)
+netto.grid(row=2,column=1)
+
+Label(window,text="össz adó érték").grid(row=3,column=0)
+osszado = Entry(window)
+osszado.grid(row=3,column=1)
+
+window.mainloop()
+
